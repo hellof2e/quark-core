88eb0cd5125b89a0505bfe810f43c2af46bade4c
xushunfa459 <xushunfa459@hellobike.com>
2023-02-24 15:41:28 +0800

    feat: optimize

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,27 +1,15 @@
 <script setup>
-import Desc from './components/Desc/index.vue'
+import Desc from './Desc.vue'
 </script>
 
 <template>
+  
   <div>
     <a href="https://quark-design.hellobike.com" target="_blank">
       <img src="https://quark-design.hellobike.com/assets/quark-logo.7fd50e67.png" class="logo" alt="Vite logo" />
     </a>
   </div>
+  
   <Desc />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</template>
--- example/src/Desc.vue
+++ example/src/Desc.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h2>Quark - 下一代前端组件框架</h2>
+    
+    <!-- Step 2. Use it -->
     <quark-count />
   </div>
 </template>
 <script>
-  import '../Count'
+  // Step 1. import
+  import './components/count.tsx'
 </script>
