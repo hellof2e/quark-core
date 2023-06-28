c165b48bc63735c530cf124163f44e684dde3eb9
duyifei121 <duyifei121@hellobike.com>
2023-06-28 13:04:21 +0800

    feat: update package.json

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,12 +1,7 @@
 const presets = [["@babel/preset-env"]];
 const plugins = [
   ["@babel/plugin-transform-runtime"],
-  [
-    "@babel/plugin-proposal-decorators",
-    {
-      legacy: true,
-    },
-  ],
+  ["@babel/plugin-proposal-decorators", { "version": "legacy" }],
 ];
 
 export default { presets, plugins };
