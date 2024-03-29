df19a786ead70029464d2c09076805ef4bf2e2be
nieyt <nieyt123@gmail.com>
2024-03-29 19:16:07 +0800

    core(router): edit umd build

--- packages/router/vite.config.build-umd.ts
+++ packages/router/vite.config.build-umd.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts'; // 自动生成 .d.ts 类型声明文件
 
 
 export default defineConfig({
-  plugins: [
-    dts({
-      outDir: 'lib',
-    }),
-  ],
   build: {
     lib: {
       entry: "./src/index.ts",
