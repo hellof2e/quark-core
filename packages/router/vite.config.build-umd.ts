060059b191a692011d882f4008c1ae236864a983
nieyt <nieyt123@gmail.com>
2024-04-12 16:33:41 +0800

    feat(router): add files

---
+++ packages/router/vite.config.build-umd.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      formats: ['umd'],
+      fileName: (format, entryName) => {
+        return `${entryName}.${format}.js`;
+      },
+      name: 'quarkRouter'
+    },
+    rollupOptions: {
+      external: [
+        '@babel/runtime',
+        'quarkc',
+      ],
+      output: {
+        dir: "umd",
+        globals: {
+          quarkc: 'Quarkc',
+        },
+      },
+      plugins: [
+      ],
+    },
+  },
+});
