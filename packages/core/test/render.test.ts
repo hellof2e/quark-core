d6073ea798cf714f0b4ea6eb600eee0bb4f4d7e5
dyf19118 <jennis19118@gmail.com>
2024-04-24 00:25:05 +0800

    test(render): simplify & add nested nodes test case

--- packages/core/test/render.test.ts
+++ packages/core/test/render.test.ts
@@ -14,11 +14,13 @@ describe('render', async () => {
   const ROOT_HTML = '<div></div>';
   const root = await fixture(ROOT_HTML);
   const render = renderHelper(root);
+
   it('render text into root', () => {
     render(WELCOME);
     expect(root.textContent).to.equal(WELCOME);
     expect(root.outerHTML).to.equal(`<div>${WELCOME}</div>`);
   });
+
   it('render node into root', () => {
     const node = createElement('div', {}, [WELCOME]);
     const child = render(node);
@@ -28,29 +30,28 @@ describe('render', async () => {
     expect(root.innerHTML).to.equal(HTML);
     expect(root.outerHTML).to.equal(`<div>${HTML}</div>`);
   });
-  it('modify text content', () => {
-    const modifiedText = WELCOME.slice(1, -1)
-    const node = createElement('div', {}, [modifiedText]);
-    const child = render(node);
-    expect(child?.textContent).to.equal(modifiedText);
-  });
-  it('add attribute', () => {
+
+  it('attribute', () => {
     const node = createElement('div', { title: '123' });
     const child = render(node);
     expect(child?.getAttribute('title')).to.equal('123');
   });
-  it('modify attribute', () => {
-    const node = createElement('div', { title: '12' });
+
+  it('nested and reused nodes', () => {
+    const para = createElement('p', { className: 'para' }, ['lorem']);
+    const node = createElement('div', {}, [
+      para,
+      WELCOME,
+      para,
+    ]);
     const child = render(node);
-    expect(child?.getAttribute('title')).to.equal('12');
+    expect(child?.childNodes.length).to.equal(3);
+    const paraElems = child?.querySelectorAll('.para');
+    expect(paraElems).to.exist;
+    expect(paraElems?.length).to.equal(2);
+    expect(paraElems?.[1].textContent).to.equal('lorem');
   });
-  it('change node type', () => {
-    const node = createElement('p', {}, [WELCOME]);
-    const child = render(node);
-    expect(child?.nodeName).to.equal('P');
-    expect(child?.getAttribute('title')).to.not.exist;
-    expect(child?.textContent).to.equal(WELCOME);
-  });
+
   it('render null into root', () => {
     render(null);
     expect(root.outerHTML).to.equal(ROOT_HTML);
