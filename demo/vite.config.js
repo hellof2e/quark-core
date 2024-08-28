8c6a2abc2690128ca34c03a25570cdb4d8ca9bd6
xsf0105 <allan91coder@gmail.com>
2024-08-28 11:26:44 +0800

    feat: add vite-plugin-dev-inspector to demo

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import { resolve }from 'path'
+import inspector from 'vite-plugin-dev-inspector'
 
 // https://vitejs.dev/config/
 const rootPtah = resolve(__dirname, "../");
 export default defineConfig({
   rootPtah,
   base: "./",
+  plugins: [
+    inspector({
+      toggleButtonVisibility: 'never'
+    }),
+  ],
   resolve: {
 		alias: [
 			{
